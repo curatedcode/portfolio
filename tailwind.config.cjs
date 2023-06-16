48b4f4b730ca5b0759e8b7770b69a1e265d25563
Zack F <zackaryfotheringham@hotmail.com>
2023-06-15 23:48:03 -0400

    add header section with social links

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "site-blue": "#0f172a",
+      },
+      height: {
+        "screen-minus-nav": "calc(100vh - 10rem)",
+      },
+      fontFamily: {
+        inter: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
