a421a9ab087400f9e8ba94fe884ae4ddaa57a386
Zack <me@zackaryf.com>
2025-11-04 11:01:14 -0500

    chore: remove unused packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,13 @@
 import alpinejs from "@astrojs/alpinejs";
 import tailwindcss from "@tailwindcss/vite";
-import expressiveCode from "astro-expressive-code";
 import { defineConfig } from "astro/config";
-import rehypeMermaid from "rehype-mermaid";
-
-const mermaidCustom = () =>
-	rehypeMermaid({
-		mermaidConfig: {
-			theme: "default", // or 'neutral', 'base', 'dark'
-			themeCSS: `
-			#mermaid-0 .cluster rect {
-				fill: none !important;
-			}
-		`
-		}
-	});
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()]
 	},
-	markdown: {
-		rehypePlugins: [mermaidCustom],
-		syntaxHighlight: false
-	},
 	integrations: [
-		alpinejs({ entrypoint: "/src/entrypoint" }),
-		expressiveCode({ themes: ["github-dark"] })
+		alpinejs({ entrypoint: "/src/entrypoint" })
 	]
 });
