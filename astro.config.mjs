82ff0757c56a9cf4efb5479ea2b3b68e3c4b9366
Zack <me@zackaryf.com>
2025-11-22 14:05:50 -0500

    chore: use vite-pwa plugin to generate manifest and service worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,52 @@
 import alpinejs from "@astrojs/alpinejs";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()]
 	},
-	integrations: [alpinejs({ entrypoint: "/src/entrypoint" })]
+	integrations: [
+		alpinejs({ entrypoint: "/src/entrypoint" }),
+		AstroPWA({
+			manifest: {
+				name: "Zackary Fotheringham",
+				short_name: "Zack F.",
+				description:
+					"Portfolio of Zackary Fotheringham, a software engineer specializing in intuitive UI/UX, and fast, accessible websites.",
+				theme_color: "#0a192f",
+				background_color: "#0a192f",
+				display: "minimal-ui",
+				id: "/",
+				scope: "/",
+				start_url: "/",
+				icons: [
+					{
+						src: "/assets/icons/192x192.png",
+						sizes: "192x192",
+						type: "image/png"
+					},
+					{
+						src: "/assets/icons/512x512.png",
+						sizes: "512x512",
+						type: "image/png"
+					},
+					{
+						src: "/assets/icons/512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any"
+					},
+					{
+						src: "/assets/icons/512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "maskable"
+					}
+				]
+			}
+		})
+	]
 });
